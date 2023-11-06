7af04d2784b5b3e4000f08e71a96fd1a131b5244
BOUDEFFAZAKARYA <boudeffazack@gmail.com>
2023-11-06 18:03:38 +0100

    fix a small error in new product page

--- app/(dashboard)/dashboard/stores/[storeId]/products/new/page.tsx
+++ app/(dashboard)/dashboard/stores/[storeId]/products/new/page.tsx
@@ -1,14 +1,9 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
 
-export class page extends Component {
-  static propTypes = {}
-
-  render() {
-    return (
-      <div>page</div>
-    )
-  }
+function page() {
+  return (
+    <div>page</div>
+  )
 }
 
 export default page
